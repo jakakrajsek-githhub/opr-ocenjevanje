2bf10222912563cf55ac2de6e541de647cbb594b
jakakrajsek <jaka.krajsek@icloud.com>
2026-03-19 22:46:48 +0100

    konc

--- classLibrary/Prostor.cs
+++ classLibrary/Prostor.cs
@@ -15,7 +15,7 @@ namespace classLibrary
 
         private string _imeProstora;
 
-        public string ImeProstora  //Lastnost
+        public string ImeProstora  //Lastnost private in public
         {
             get { return _imeProstora; }
             set
